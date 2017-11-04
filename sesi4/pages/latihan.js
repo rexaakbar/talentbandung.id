598191ced65302b8d3bd3025a2f420e9bc5c23f4
rexaakbar <akbarrexa@gmail.com>
2017-11-04 14:02:26 +0700

    add project sesi4

---
+++ sesi4/pages/latihan.js
@@ -0,0 +1,153 @@
+// var nama = "Rexa Akbar Malik";
+// var alamat = null;
+
+// var webBasic = ['HTML' , 'CSS' , 'JavaScript' , 'PHP'];
+
+// for(a = 0;a < 4 ; a++){
+// 	webBasic[a];
+// }
+
+// alert("Hellow Hello");
+
+
+// var namaBuah = ["Jambu" , "Mangga" , "Sirsak" , "Strawberry" , "Jeruk" , "Menggkudu" , "Rambutan"];
+
+// console.log(namaBuah[4]);
+
+// namaBuah[4] = "Semanka";
+
+// console.log(namaBuah[4]);
+
+// var person = {
+// 	name: "Rexa Akbar Malik",
+// 	age : "18",
+// 	address : "Jamika",
+// 	gadgets:{
+// 		laptop:"MBP",
+// 		phone :"Android"	
+// 	},
+// 	hobby:['Sleep' , 'Code']
+// };
+
+// console.log(person.name);
+// console.log(person.gadgets.laptop);
+// console.log(person.hobby[0]);
+
+// var quran = {"code":200,"status":"OK","data":{"number":1,"name":"\u0633\u0648\u0631\u0629 \u0627\u0644\u0641\u0627\u062a\u062d\u0629","englishName":"Al-Faatiha","englishNameTranslation":"The Opening","revelationType":"Meccan","numberOfAyahs":7,"ayahs":[{"number":1,"text":"\ufeff\u0628\u0650\u0633\u0652\u0645\u0650 \u0627\u0644\u0644\u0651\u064e\u0647\u0650 \u0627\u0644\u0631\u0651\u064e\u062d\u0652\u0645\u064e\u0670\u0646\u0650 \u0627\u0644\u0631\u0651\u064e\u062d\u0650\u064a\u0645\u0650","numberInSurah":1,"juz":1,"manzil":1,"page":1,"ruku":1,"hizbQuarter":1,"sajda":false},{"number":2,"text":"\u0627\u0644\u0652\u062d\u064e\u0645\u0652\u062f\u064f \u0644\u0650\u0644\u0651\u064e\u0647\u0650 \u0631\u064e\u0628\u0651\u0650 \u0627\u0644\u0652\u0639\u064e\u0627\u0644\u064e\u0645\u0650\u064a\u0646\u064e","numberInSurah":2,"juz":1,"manzil":1,"page":1,"ruku":1,"hizbQuarter":1,"sajda":false},{"number":3,"text":"\u0627\u0644\u0631\u0651\u064e\u062d\u0652\u0645\u064e\u0670\u0646\u0650 \u0627\u0644\u0631\u0651\u064e\u062d\u0650\u064a\u0645\u0650","numberInSurah":3,"juz":1,"manzil":1,"page":1,"ruku":1,"hizbQuarter":1,"sajda":false},{"number":4,"text":"\u0645\u064e\u0627\u0644\u0650\u0643\u0650 \u064a\u064e\u0648\u0652\u0645\u0650 \u0627\u0644\u062f\u0651\u0650\u064a\u0646\u0650","numberInSurah":4,"juz":1,"manzil":1,"page":1,"ruku":1,"hizbQuarter":1,"sajda":false},{"number":5,"text":"\u0625\u0650\u064a\u0651\u064e\u0627\u0643\u064e \u0646\u064e\u0639\u0652\u0628\u064f\u062f\u064f \u0648\u064e\u0625\u0650\u064a\u0651\u064e\u0627\u0643\u064e \u0646\u064e\u0633\u0652\u062a\u064e\u0639\u0650\u064a\u0646\u064f","numberInSurah":5,"juz":1,"manzil":1,"page":1,"ruku":1,"hizbQuarter":1,"sajda":false},{"number":6,"text":"\u0627\u0647\u0652\u062f\u0650\u0646\u064e\u0627 \u0627\u0644\u0635\u0651\u0650\u0631\u064e\u0627\u0637\u064e \u0627\u0644\u0652\u0645\u064f\u0633\u0652\u062a\u064e\u0642\u0650\u064a\u0645\u064e","numberInSurah":6,"juz":1,"manzil":1,"page":1,"ruku":1,"hizbQuarter":1,"sajda":false},{"number":7,"text":"\u0635\u0650\u0631\u064e\u0627\u0637\u064e \u0627\u0644\u0651\u064e\u0630\u0650\u064a\u0646\u064e \u0623\u064e\u0646\u0652\u0639\u064e\u0645\u0652\u062a\u064e \u0639\u064e\u0644\u064e\u064a\u0652\u0647\u0650\u0645\u0652 \u063a\u064e\u064a\u0652\u0631\u0650 \u0627\u0644\u0652\u0645\u064e\u063a\u0652\u0636\u064f\u0648\u0628\u0650 \u0639\u064e\u0644\u064e\u064a\u0652\u0647\u0650\u0645\u0652 \u0648\u064e\u0644\u064e\u0627 \u0627\u0644\u0636\u0651\u064e\u0627\u0644\u0651\u0650\u064a\u0646\u064e","numberInSurah":7,"juz":1,"manzil":1,"page":1,"ruku":1,"hizbQuarter":1,"sajda":false}],"edition":{"identifier":"quran-simple","language":"ar","name":"Simple","englishName":"Simple","format":"text","type":"quran"}}};
+
+// console.log(quran.data.name);
+// console.log(quran.data.ayahs[0].text);
+// console.log(quran.data.ayahs[1].text);
+// console.log(quran.data.ayahs[2].text);
+// console.log(quran.data.ayahs[3].text);
+// console.log(quran.data.ayahs[4].text);
+// console.log(quran.data.ayahs[5].text);
+// console.log(quran.data.ayahs[6].text);
+
+// var nilai = 8;
+
+// if(nilai >= 8){
+// 	console.log("Selamat Anda Lulus Dengan Nilai " + nilai);
+// }else if(nilai >= 6){
+// 	console.log("Selamat Anda Lulus Dengan Nilai Pas Pasan");
+// }else{
+// 	console.log("Selamat Anda Tidak Lulus");
+// }
+
+// var jam = 24;
+
+// if(jam >= 1 && jam <= 11){
+// 	alert("Selamat Pagi");
+// }else if(jam >= 12 && jam <= 15){
+// 	alert("Selamat Siang");
+// }else if(jam >= 16 && jam <= 18){
+// 	alert("Selamat Sore");
+// }else if(jam >= 19 && jam <= 24){
+// 	alert("Selamat Malam");
+// }else{
+// 	alert("Inputan Anda Salah");	
+// }
+
+// var buah = "jeruk";
+// switch ( buah ) {
+//     case "jeruk":
+//         alert("Warna buah jeruk itu orange/kuning");
+//         break;
+//     case "apel":
+//         alert("Warna buah apel itu merah/kuning");
+//         break;
+//     default:
+//         alert("Nama buah tidak dikenal");
+// }
+
+
+
+// var hari = ;
+
+// switch(hari) {
+// 	case 0 :
+// 		alert("Senin");
+// 		break;
+// 	case 1 :
+// 		alert("Selasa");
+// 		break;
+// 	case 2 :
+// 		alert("Rabu");
+// 		break;
+// 	case 3 :
+// 		alert("Kamis");
+// 		break;
+// 	case 4 :
+// 		alert("Jumat");
+// 		break;
+// 	case 5 :
+// 		alert("Sabtu");
+// 		break;
+// 	case 6 :
+// 		alert("Minggu");
+// 		break;
+// 	default:
+// 		alert("Test");
+// 		break;
+// }
+
+// for ( i=1; i <= 100; ++i) {
+//     console.log( 'Perulangan ke-' + i );
+// }
+
+// var i = 0;
+// while ( i <= 100 ) {
+//     console.log( 'Perulangan ke-' + i );
+// 	i++;
+// }
+
+// var quran = {"code":200,"status":"OK","data":{"number":1,"name":"\u0633\u0648\u0631\u0629 \u0627\u0644\u0641\u0627\u062a\u062d\u0629","englishName":"Al-Faatiha","englishNameTranslation":"The Opening","revelationType":"Meccan","numberOfAyahs":7,"ayahs":[{"number":1,"text":"\ufeff\u0628\u0650\u0633\u0652\u0645\u0650 \u0627\u0644\u0644\u0651\u064e\u0647\u0650 \u0627\u0644\u0631\u0651\u064e\u062d\u0652\u0645\u064e\u0670\u0646\u0650 \u0627\u0644\u0631\u0651\u064e\u062d\u0650\u064a\u0645\u0650","numberInSurah":1,"juz":1,"manzil":1,"page":1,"ruku":1,"hizbQuarter":1,"sajda":false},{"number":2,"text":"\u0627\u0644\u0652\u062d\u064e\u0645\u0652\u062f\u064f \u0644\u0650\u0644\u0651\u064e\u0647\u0650 \u0631\u064e\u0628\u0651\u0650 \u0627\u0644\u0652\u0639\u064e\u0627\u0644\u064e\u0645\u0650\u064a\u0646\u064e","numberInSurah":2,"juz":1,"manzil":1,"page":1,"ruku":1,"hizbQuarter":1,"sajda":false},{"number":3,"text":"\u0627\u0644\u0631\u0651\u064e\u062d\u0652\u0645\u064e\u0670\u0646\u0650 \u0627\u0644\u0631\u0651\u064e\u062d\u0650\u064a\u0645\u0650","numberInSurah":3,"juz":1,"manzil":1,"page":1,"ruku":1,"hizbQuarter":1,"sajda":false},{"number":4,"text":"\u0645\u064e\u0627\u0644\u0650\u0643\u0650 \u064a\u064e\u0648\u0652\u0645\u0650 \u0627\u0644\u062f\u0651\u0650\u064a\u0646\u0650","numberInSurah":4,"juz":1,"manzil":1,"page":1,"ruku":1,"hizbQuarter":1,"sajda":false},{"number":5,"text":"\u0625\u0650\u064a\u0651\u064e\u0627\u0643\u064e \u0646\u064e\u0639\u0652\u0628\u064f\u062f\u064f \u0648\u064e\u0625\u0650\u064a\u0651\u064e\u0627\u0643\u064e \u0646\u064e\u0633\u0652\u062a\u064e\u0639\u0650\u064a\u0646\u064f","numberInSurah":5,"juz":1,"manzil":1,"page":1,"ruku":1,"hizbQuarter":1,"sajda":false},{"number":6,"text":"\u0627\u0647\u0652\u062f\u0650\u0646\u064e\u0627 \u0627\u0644\u0635\u0651\u0650\u0631\u064e\u0627\u0637\u064e \u0627\u0644\u0652\u0645\u064f\u0633\u0652\u062a\u064e\u0642\u0650\u064a\u0645\u064e","numberInSurah":6,"juz":1,"manzil":1,"page":1,"ruku":1,"hizbQuarter":1,"sajda":false},{"number":7,"text":"\u0635\u0650\u0631\u064e\u0627\u0637\u064e \u0627\u0644\u0651\u064e\u0630\u0650\u064a\u0646\u064e \u0623\u064e\u0646\u0652\u0639\u064e\u0645\u0652\u062a\u064e \u0639\u064e\u0644\u064e\u064a\u0652\u0647\u0650\u0645\u0652 \u063a\u064e\u064a\u0652\u0631\u0650 \u0627\u0644\u0652\u0645\u064e\u063a\u0652\u0636\u064f\u0648\u0628\u0650 \u0639\u064e\u0644\u064e\u064a\u0652\u0647\u0650\u0645\u0652 \u0648\u064e\u0644\u064e\u0627 \u0627\u0644\u0636\u0651\u064e\u0627\u0644\u0651\u0650\u064a\u0646\u064e","numberInSurah":7,"juz":1,"manzil":1,"page":1,"ruku":1,"hizbQuarter":1,"sajda":false}],"edition":{"identifier":"quran-simple","language":"ar","name":"Simple","englishName":"Simple","format":"text","type":"quran"}}};
+
+// for (var i = 0; i < quran.data.ayahs.length; i++) {
+// 	console.log( quran.data.ayahs[i].text );
+// }
+
+// var buah = ['Jambu' , 'Mangga' , 'Anggur' , 'Apel' , 'Semangka' ];
+
+// for (var i = 0; i < buah.length; i++) {
+// 	console.log(buah[i]);
+// }
+
+// function luasPersegiPanjang( panjang, lebar ) {
+//     return panjang * lebar;
+// }
+// // contoh penggunaan
+// luasPersegiPanjang( 10, 5 );       // 50
+
+// var colNama = ['Rexa' , 'Akbar' , 'Malik' , 'Marcell' , 'Sukma' , 'Melati'];
+
+// for (var i = 0; i < colNama.length; i++) {
+// 	greet(colNama[i]);
+// }
+
+function greet(nama = "Nama Belum Di Isi"){
+	return alert("Selamat malam tuan " + nama);
+	// return console.log("Selamat malam " + nama);
+}
+
+greet("Rexa Akbar Malik");
